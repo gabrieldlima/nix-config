a2166c8efae547fda91067834cf097b9cb7cf825
Gabriel Lima <gabrielgsdlima@gmail.com>
2023-11-06 17:21:04 -0300

    Standalone home-manager configuration entrypoint

--- flake.nix
+++ flake.nix
@@ -31,12 +31,11 @@
     # Standalone home-manager configuration entrypoint
     # Available through 'home-manager --flake .#your-username@your-hostname'
     homeConfigurations = {
-      # FIXME replace with your username@hostname
-      "your-username@your-hostname" = home-manager.lib.homeManagerConfiguration {
+      "gabriel@aorus" = home-manager.lib.homeManagerConfiguration {
         pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
-        extraSpecialArgs = {inherit inputs outputs;};
+        extraSpecialArgs = { inherit inputs outputs; };
         # > Our main home-manager configuration file <
-        modules = [./home-manager/home.nix];
+        modules = [ ./home-manager/home.nix ];
       };
     };
   };
