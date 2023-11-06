fe6b28eb673f32f9e418a488da44151487e3cf85
Gabriel Lima <gabrielgsdlima@gmail.com>
2023-11-06 17:17:21 -0300

    Personal NixOS Flake. Using the unstable branch in nixpkgs and home-manager

--- flake.nix
+++ flake.nix
@@ -1,20 +1,13 @@
 {
-  description = "Your new nix config";
+  description = "Personal NixOS Flake";
 
   inputs = {
     # Nixpkgs
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
     # Home manager
-    home-manager.url = "github:nix-community/home-manager/release-23.05";
+    home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
-
-    # TODO: Add any other flake you might need
-    # hardware.url = "github:nixos/nixos-hardware";
-
-    # Shameless plug: looking for a way to nixify your themes and make
-    # everything match nicely? Try nix-colors!
-    # nix-colors.url = "github:misterio77/nix-colors";
   };
 
   outputs = {
