f58be2137b0d9ac0d5a81ae4946b2a625e1f2843
Gabriel Lima <gabrielgsdlima@gmail.com>
2023-11-19 18:12:55 -0300

    Fish shell plugins: z (jump dir), fzf.fish (fuzzy finder for fish)

--- home/gabriel/programs/fish.nix
+++ home/gabriel/programs/fish.nix
@@ -5,5 +5,28 @@
   programs.fish = {
     enable = true;
     interactiveShellInit = "starship init fish | source";
+    plugins = [
+      # Pure-fish z directory jumping
+      {
+        name = "z";
+        src = pkgs.fetchFromGitHub {
+          owner = "jethrokuan";
+          repo = "z";
+          rev = "85f863f20f24faf675827fb00f3a4e15c7838d76";
+          sha256 = "sha256-+FUBM7CodtZrYKqU542fQD+ZDGrd2438trKM0tIESs0=";
+        };
+      }
+
+      # Fzf plugin for Fish
+      {
+        name = "fzf.fish";
+        src = pkgs.fetchFromGitHub {
+          owner = "PatrickF1";
+          repo = "fzf.fish";
+          rev = "46c7bc6354494be5d869d56a24a46823a9fdded0";
+          sha256 = "sha256-lxQZo6APemNjt2c21IL7+uY3YVs81nuaRUL7NDMcB6s=";
+        };
+      }
+    ];
   };
 }
