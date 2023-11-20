ff60da7e3c4f702fb31b64e0d4fd304b625f2fa0
Gabriel Lima <gabrielgsdlima@gmail.com>
2023-11-20 19:02:50 -0300

    Catppuccin colors for starship prompt

--- home/gabriel/programs/starship.nix
+++ home/gabriel/programs/starship.nix
@@ -1,8 +1,23 @@
 {
   pkgs,
+  lib,
   ...
 }: {
-  programs.starship = {
-    enable = true;
-  };
+  programs.starship =
+    let
+      flavour = "mocha";
+    in
+    {
+      enable = true;
+      settings = {
+        palette = "catppuccin_${flavour}";
+      } // builtins.fromTOML (builtins.readFile
+        (pkgs.fetchFromGitHub
+          {
+            owner = "catppuccin";
+            repo = "starship";
+            rev = "5629d2356f62a9f2f8efad3ff37476c19969bd4f";
+            sha256 = "sha256-nsRuxQFKbQkyEI4TXgvAjcroVdG+heKX5Pauq/4Ota0=";
+          } + /palettes/${flavour}.toml));
+    };
 }
