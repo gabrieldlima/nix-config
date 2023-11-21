f66ef8445f6d8c5ca7eb13b63c81f1a15829c6b7
Gabriel Lima <gabrielgsdlima@gmail.com>
2023-11-21 19:24:46 -0300

    Nerd Font Symbols preset

--- home/gabriel/programs/starship.nix
+++ home/gabriel/programs/starship.nix
@@ -16,6 +16,40 @@
           success_symbol = "[](green)";
           error_symbol = "[](red)";
         };
+
+        # Nerd Font icons
+        aws.symbol = " ";
+        buf.symbol = " ";
+        c.symbol = " ";
+        conda.symbol = " ";
+        dart.symbol = " ";
+        directory.read_only = " 󰌾";
+        docker_context.symbol = " ";
+        elixir.symbol = " ";
+        elm.symbol = " ";
+        fossil_branch.symbol = " ";
+        git_branch.symbol = " ";
+        golang.symbol = " ";
+        guix_shell.symbol = " ";
+        haskell.symbol = " ";
+        haxe.symbol = " ";
+        hg_branch.symbol = " ";
+        hostname.ssh_symbol = " ";
+        java.symbol = " ";
+        julia.symbol = " ";
+        lua.symbol = " ";
+        memory_usage.symbol = "󰍛 ";
+        meson.symbol = "󰔷 ";
+        nim.symbol = "󰆥 ";
+        nix_shell.symbol = " ";
+        nodejs.symbol = " ";
+        package.symbol = "󰏗 ";
+        pijul_channel.symbol = " ";
+        python.symbol = " ";
+        rlang.symbol = "󰟔 ";
+        ruby.symbol = " ";
+        rust.symbol = " ";
+        scala.symbol = " ";
       } // builtins.fromTOML (builtins.readFile
         (pkgs.fetchFromGitHub
           {
