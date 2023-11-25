654ffd9da208e607421f1056b31849a13b294f33
Gabriel Lima <gabrielgsdlima@gmail.com>
2023-11-25 15:34:56 -0300

    Enable xmobar

--- home/gabriel/programs/xmonad/default.nix
+++ home/gabriel/programs/xmonad/default.nix
@@ -2,8 +2,14 @@
   pkgs,
   ...
 }: {
+  # Xmonad and Xmonad-Contrib
   xsession.windowManager.xmonad = {
     enable = true;
     enableContribAndExtras = true;
+  };
+
+  # Xmobar
+  programs.xmobar = {
+    enable = true;
   };
 }
