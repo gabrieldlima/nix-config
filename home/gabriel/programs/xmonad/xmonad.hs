8aa30a983e2becdb502a298b0e958541bee92e0b
Gabriel Lima <gabrielgsdlima@gmail.com>
2023-11-25 16:31:06 -0300

    Define some default keybindings

--- home/gabriel/programs/xmonad/xmonad.hs
+++ home/gabriel/programs/xmonad/xmonad.hs
@@ -4,4 +4,13 @@ import XMonad.Util.EZConfig
 import XMonad.Util.Ungrab
 
 main :: IO ()
-main = xmonad def
+main = xmonad $ def
+    {
+      modMask = mod4Mask -- Rebind Mod to the Super Key
+    }
+  `additionalKeysP`
+    [
+      ("M-b", spawn "qutebrowser")     -- Browser
+    , ("M-p", spawn "rofi -show drun") -- Launcher
+    , ("M-<Return>", spawn "wezterm")  -- Terminal
+    ]
