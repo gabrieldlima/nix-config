50dc36026cb93fe6211cd5762856663bcbb77b53
Gabriel Lima <gabrielgsdlima@gmail.com>
2023-11-26 18:18:07 -0300

    Xmobar plugin: CPU and Memory

--- home/gabriel/programs/xmonad/xmobar.nix
+++ home/gabriel/programs/xmonad/xmobar.nix
@@ -16,10 +16,26 @@
                           Run Date "<fc=#cba6f7>󰥔 </fc>%I:%M %p" "time" 10
                         , Run Date "<fc=#a6e3a1>󰃭 </fc>%a %b %d" "date" 10
                         , Run XMonadLog
+                        , Run Cpu
+                            [
+                              "--template", "<fc=#b4befe> </fc><total>%"
+                            , "--Low", "3"
+                            , "--High", "50"
+                            , "--low", "#cdd6f4"
+                            , "--normal", "#cdd6f4"
+                            , "--high", "#f38ba8"
+                            ] 10
+                        , Run Memory
+                            [
+                              "--template", "<fc=#f2cdcd>󰘚 </fc><used>GB"
+                            , "--ddigits", "1"
+                            , "--"
+                            , "--scale", "1024"
+                            ] 10
                         ]
            , sepChar  = "%"
            , alignSep = "}{"
-           , template = "%XMonadLog% }{ %date% | %time%"
+           , template = "%XMonadLog% }{ %cpu% | %memory% | %date% | %time%"
            }
     '';
   };
