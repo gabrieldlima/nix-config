aaa17cd4d0d19a0737afe9d4b3b34685e3e48ab9
Gabriel Lima <gabrielgsdlima@gmail.com>
2023-11-26 17:16:20 -0300

    Xmobar plugin: Date monitors

--- home/gabriel/programs/xmonad/xmobar.nix
+++ home/gabriel/programs/xmonad/xmobar.nix
@@ -6,40 +6,20 @@
     enable = true;
     extraConfig = ''
     Config {
-             overrideRedirect = False
-           , font     = "Sugar Snow Regular 11"
+             font     = "Sugar Snow Regular 11"
            , bgColor  = "#050508"
            , fgColor  = "#cdd6f4"
-           , position = TopW L 100
+           , alpha    = 255
+           , position = TopH 30
+           , overrideRedirect = False
            , commands = [
-                          Run Weather "EGPF"
-                            [ "--template", "<weather> <tempC>°C"
-                            , "-L", "0"
-                            , "-H", "25"
-                            , "--low"   , "lightblue"
-                            , "--normal", "#f8f8f2"
-                            , "--high"  , "red"
-                            ] 36000
-                        , Run Cpu
-                            [ "-L", "3"
-                            , "-H", "50"
-                            , "--high"  , "red"
-                            , "--normal", "green"
-                            ] 10
-                        , Run Alsa "default" "Master"
-                            [ "--template", "<volumestatus>"
-                            , "--suffix"  , "True"
-                            , "--"
-                            , "--on", ""
-                            ]
-                        , Run Memory ["--template", "Mem: <usedratio>%"] 10
-                        , Run Swap [] 10
-                        , Run Date "%a %Y-%m-%d <fc=#8be9fd>%H:%M</fc>" "date" 10
+                          Run Date "<fc=#cba6f7>󰥔 </fc>%I:%M %p" "time" 10
+                        , Run Date "<fc=#a6e3a1>󰃭 </fc>%a %b %d" "date" 10
                         , Run XMonadLog
                         ]
            , sepChar  = "%"
            , alignSep = "}{"
-           , template = "%XMonadLog% }{ %alsa:default:Master% | %cpu% | %memory% * %swap% | %EGPF% | %date% "
+           , template = "%XMonadLog% }{ %date% | %time%"
            }
     '';
   };
