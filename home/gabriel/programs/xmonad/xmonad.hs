1b0ef054be1a88d523166456edbf4e8d90a34bf6
Gabriel Lima <gabrielgsdlima@gmail.com>
2023-11-26 10:27:51 -0300

    Enhances XMonad's handling of EWMH hints and full-screen support

--- home/gabriel/programs/xmonad/xmonad.hs
+++ home/gabriel/programs/xmonad/xmonad.hs
@@ -9,13 +9,17 @@
 
 import XMonad                     -- Core XMonad module
 
+import XMonad.Hooks.EwmhDesktops  -- Enhances XMonad's handling of EWMH hints and full-screen support
+
 import XMonad.Layout.ThreeColumns -- Layout for arranging windows in three columns
 
 import XMonad.Util.EZConfig       -- Utility for easily configuring keybindings
 import XMonad.Util.Ungrab         -- Handles releasing keyboard and pointer grabs
 
 main :: IO ()
-main = xmonad $ def
+main = xmonad $ ewmhFullscreen $ ewmh $ myConfig
+
+myConfig = def
     {
       modMask = mod4Mask    -- Rebind Mod to the Super Key
     , layoutHook = myLayout -- Use custom layouts
