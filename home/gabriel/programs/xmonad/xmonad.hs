2578646fceac6086bdc760391407733f93a264fd
Gabriel Lima <gabrielgsdlima@gmail.com>
2023-12-03 17:48:28 -0300

    A way better config with code docs

--- home/gabriel/programs/xmonad/xmonad.hs
+++ home/gabriel/programs/xmonad/xmonad.hs
@@ -7,27 +7,19 @@
 -- ║╚═╝  ╚═╝╚═╝     ╚═╝ ╚═════╝ ╚═╝  ╚═══╝╚═╝  ╚═╝╚═════╝ ║
 -- ╚══════════════════════════════════════════════════════╝
 
+
 --
 -- Useful imports
 --
 import XMonad                     -- Core XMonad module
 
 import XMonad.Hooks.EwmhDesktops  -- Enhances XMonad's handling of EWMH hints and full-screen support
-import XMonad.Hooks.DynamicLog    -- Send information about xmonad's state to an X11 property or standard output
-import XMonad.Hooks.StatusBar     -- Composable and dynamic status bars
-import XMonad.Hooks.StatusBar.PP  -- The pretty-printing abstraction for handling status bars
 import XMonad.Hooks.ManageHelpers -- Provides helper functions to be used in manageHook
-
-import XMonad.Layout.ThreeColumns -- Layout for arranging windows in three columns
-import XMonad.Layout.Spacing      -- Add a configurable amount of space around windows
-import XMonad.Layout.Renamed      -- Modify the description of a layout in a flexible way
-import XMonad.Layout.NoBorders    -- Make a given layout display without borders
-
 import XMonad.ManageHook          -- An EDSL for ManageHooks
-
 import XMonad.Util.EZConfig       -- Utility for easily configuring keybindings
 import XMonad.Util.Ungrab         -- Handles releasing keyboard and pointer grabs
 import XMonad.Util.SpawnOnce      -- A module for spawning a command once, and only once
+
 
 -------------------------------------------------------------------------------
 -- The default number of workspaces (virtual screens) and their names.
@@ -71,6 +63,7 @@ myFocusFollowsMouse = True
 myClickJustFocuses :: Bool
 myClickJustFocuses = True
 
+
 -------------------------------------------------------------------------------
 -- Window rules
 
@@ -87,6 +80,7 @@ myManageHook = composeAll
     [
       isDialog --> doFloat
     ]
+
 
 -------------------------------------------------------------------------------
 -- Key bindings:
@@ -111,7 +105,12 @@ myKeys =
     , ("M-C-s",      spawn myScreenshot) -- Launch scrot
     ]
 
+
 -------------------------------------------------------------------------------
+-- Startup hook:
+
+-- Perform an arbitrary action each time xmonad starts or is restarted
+-- with mod-q.
 myStartupHook :: X ()
 myStartupHook = do
     spawnOnce "picom"                          -- Compositor
@@ -121,34 +120,55 @@ myStartupHook = do
     spawnOnce "openrgb -d 0 -m off"            -- Turn off DIMM01 RGB leds
     spawnOnce "openrgb -d 1 -m off"            -- Turn off DIMM02 RGB leds
 
--------------------------------------------------------------------------------
-myLayoutHook = renamed [CutWordsLeft 1] $ spacingRaw False (Border 5 5 5 5) True (Border 5 5 5 5) True
-    $ smartBorders $ tiled ||| Mirror tiled ||| threeCol ||| Full
-  where
-    threeCol = ThreeColMid nmaster delta ratio
-    tiled   = Tall nmaster delta ratio
-    nmaster = 1     -- Default number of windows in the master pane
-    ratio   = 1/2   -- Default proportion of screen occupied by master pane
-    delta   = 3/100 -- Percent of screen to increment by when resizing panes
 
 -------------------------------------------------------------------------------
+-- Layouts
+--
+-- You can specify and transform your layouts by modifying these values.
+-- If you change layout bindings be sure to use 'mod-shift-space' after
+-- restarting (with 'mod-q') to reset your layout state to the new
+-- defaults, as xmonad preserves your old layout settings by default.
+--
+
+-- The available layouts.  Note that each layout is separated by |||, which
+-- denotes layout choice.
+myLayoutHook = tiled ||| Mirror tiled ||| Full
+  where
+    -- default tiling algorithm partitions the screen into two panes
+    tiled   = Tall nmaster delta ratio
+
+    -- Default number of windows in the master pane
+    nmaster = 1
+
+    -- Default proportion of screen occupied by master pane
+    ratio   = 1/2
+
+    -- Percent of screen to increment by when resizing panes
+    delta   = 3/100
+
+
+-------------------------------------------------------------------------------
+-- Run xmonad with all the defaults we set up:
+
+-- A structure containing your configuration settings, overriding
+-- fields in the default config. Any you don't override, will
+-- use the defaults defined in xmonad/XMonad/Config.hs
+--
+-- No need to modify this.
+--
 myConfig = def
     {
-      modMask            = myModMask             -- Rebind Mod to the Super Key
-    , layoutHook         = myLayoutHook          -- Use custom layouts
-    , startupHook        = myStartupHook         -- Autostart applications
-    , manageHook         = myManageHook          -- Match on certain windows
-    , borderWidth        = myBorderWidth         -- Border width
-    , normalBorderColor  = myNormalBorderColor   -- Normal border color
-    , focusedBorderColor = myFocusedBorderColor  -- Focused border color
+      modMask            = myModMask
+    , layoutHook         = myLayoutHook
+    , startupHook        = myStartupHook
+    , manageHook         = myManageHook
+    , borderWidth        = myBorderWidth
+    , normalBorderColor  = myNormalBorderColor
+    , focusedBorderColor = myFocusedBorderColor
     , focusFollowsMouse  = myFocusFollowsMouse
     , clickJustFocuses   = myClickJustFocuses
     }
   `additionalKeysP` myKeys
 
--------------------------------------------------------------------------------
 main :: IO ()
-main = xmonad
-     . ewmhFullscreen
-     . ewmh
-     $ myConfig
+main = xmonad $ ewmhFullscreen $ ewmh $ myConfig
