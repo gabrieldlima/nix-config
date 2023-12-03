7162e2a3d5ffd23d5b251ace3cccfd750ea7469e
Gabriel Lima <gabrielgsdlima@gmail.com>
2023-12-03 17:33:57 -0300

    Some parts of default config template

--- home/gabriel/programs/xmonad/xmonad.hs
+++ home/gabriel/programs/xmonad/xmonad.hs
@@ -7,6 +7,9 @@
 -- ║╚═╝  ╚═╝╚═╝     ╚═╝ ╚═════╝ ╚═╝  ╚═══╝╚═╝  ╚═╝╚═════╝ ║
 -- ╚══════════════════════════════════════════════════════╝
 
+--
+-- Useful imports
+--
 import XMonad                     -- Core XMonad module
 
 import XMonad.Hooks.EwmhDesktops  -- Enhances XMonad's handling of EWMH hints and full-screen support
@@ -24,28 +27,81 @@ import XMonad.ManageHook          -- An EDSL for ManageHooks
 
 import XMonad.Util.EZConfig       -- Utility for easily configuring keybindings
 import XMonad.Util.Ungrab         -- Handles releasing keyboard and pointer grabs
-import XMonad.Util.Loggers
 import XMonad.Util.SpawnOnce      -- A module for spawning a command once, and only once
 
--- Programs
-myTerminal   = "wezterm"
-myBrowser    = "qutebrowser"
-myLauncher   = "rofi -show drun"
-myScreenshot = "scrot -s"
+-------------------------------------------------------------------------------
+-- The default number of workspaces (virtual screens) and their names.
+-- By default we use numeric strings, but any string may be used as a
+-- workspace name. The number of workspaces is determined by the length
+-- of this list.
+--
+-- A tagging example:
+--
+-- > workspaces = ["web", "irc", "code" ] ++ map show [4..9]
+--
+workspaces :: [WorkspaceId]
+workspaces = map show [1 .. 9 :: Int]
 
--- Border
-myBorderWidth        = 1
+-- modMask lets you specify which modkey you want to use. The default
+-- is mod1Mask ("left alt").  You may also consider using mod3Mask
+-- ("right alt"), which does not conflict with emacs keybindings. The
+-- "windows key" is usually mod4Mask.
+--
+myModMask :: KeyMask
+myModMask = mod4Mask
+
+-- Width of the window border in pixels.
+--
+myBorderWidth :: Dimension
+myBorderWidth = 1
+
+-- Border colors for unfocused and focused windows, respectively.
+--
+myNormalBorderColor, myFocusedBorderColor :: String
 myNormalBorderColor  = "#050508"
 myFocusedBorderColor = "#fab387"
 
-myStartupHook :: X ()
-myStartupHook = do
-    spawnOnce "picom"                          -- Compositor
-    spawnOnce "nitrogen --restore"             -- Set a nice wallpaper
-    spawnOnce "xsetroot -cursor_name left_ptr" -- Set the default X cursor to the usual pointer
-    spawnOnce "xset s off -dpms"               -- Disable screen saver
-    spawnOnce "openrgb -d 0 -m off"            -- Turn off DIMM01 RGB leds
-    spawnOnce "openrgb -d 1 -m off"            -- Turn off DIMM02 RGB leds
+-- Whether focus follows the mouse pointer.
+--
+myFocusFollowsMouse :: Bool
+myFocusFollowsMouse = True
+
+-- Whether a mouse click select the focus or is just passed to the window
+--
+myClickJustFocuses :: Bool
+myClickJustFocuses = True
+
+-------------------------------------------------------------------------------
+-- Window rules
+
+-- Execute arbitrary actions and WindowSet manipulations when managing
+-- a new window. You can use this to, for example, always float a
+-- particular program, or have a client always appear on a particular
+-- workspace.
+--
+-- To find the property name associated with a program, use
+--  xprop | grep WM_CLASS
+-- and click on the client you're interested in.
+myManageHook :: ManageHook
+myManageHook = composeAll
+    [
+      isDialog --> doFloat
+    ]
+
+-------------------------------------------------------------------------------
+-- Key bindings:
+
+myTerminal :: String
+myTerminal = "wezterm"
+
+myBrowser :: String
+myBrowser = "qutebrowser"
+
+myLauncher :: String
+myLauncher = "rofi -show drun"
+
+myScreenshot :: String
+myScreenshot = "scrot -s"
 
 myKeys =
     [
@@ -55,6 +111,17 @@ myKeys =
     , ("M-C-s",      spawn myScreenshot) -- Launch scrot
     ]
 
+-------------------------------------------------------------------------------
+myStartupHook :: X ()
+myStartupHook = do
+    spawnOnce "picom"                          -- Compositor
+    spawnOnce "nitrogen --restore"             -- Set a nice wallpaper
+    spawnOnce "xsetroot -cursor_name left_ptr" -- Set the default X cursor to the usual pointer
+    spawnOnce "xset s off -dpms"               -- Disable screen saver
+    spawnOnce "openrgb -d 0 -m off"            -- Turn off DIMM01 RGB leds
+    spawnOnce "openrgb -d 1 -m off"            -- Turn off DIMM02 RGB leds
+
+-------------------------------------------------------------------------------
 myLayoutHook = renamed [CutWordsLeft 1] $ spacingRaw False (Border 5 5 5 5) True (Border 5 5 5 5) True
     $ smartBorders $ tiled ||| Mirror tiled ||| threeCol ||| Full
   where
@@ -64,62 +131,24 @@ myLayoutHook = renamed [CutWordsLeft 1] $ spacingRaw False (Border 5 5 5 5) True
     ratio   = 1/2   -- Default proportion of screen occupied by master pane
     delta   = 3/100 -- Percent of screen to increment by when resizing panes
 
-myManageHook :: ManageHook
-myManageHook = composeAll
-    [
-      isDialog --> doFloat
-      -- className =? "WM_CLASS(STRING)" --> doFloat
-    ]
-
-myXmobarPP :: PP
-myXmobarPP = def
-    {
-      ppCurrent         = peach . wrap "[" "]"                 -- how to print the tag of the currently focused workspace
-    , ppHidden          = green . wrap "" ""                   -- how to print tags of hidden workspaces which contain windows
-    , ppUrgent          = red . wrap (yellow "!") (yellow "!") -- format to be applied to tags of urgent workspaces
-    , ppSep             = red " \xf0e7 "                       -- separator to use between different log sections
-    , ppTitle           = text . shorten 100                   -- window title format for the focused window
-    , ppTitleSanitize   = xmobarStrip                          -- escape / sanitizes input to ppTitle
-    }
-  where
-    rosewater, flamingo, pink, mauve, red, marron, peach, yellow, green, teal, sky, sapphire, blue, lavender, text, base, surface2 :: String -> String
-    rosewater = xmobarColor "#f5e0dc" ""
-    flamingo  = xmobarColor "#f2cdcd" ""
-    pink      = xmobarColor "#f5c2e7" ""
-    mauve     = xmobarColor "#cba6f7" ""
-    red       = xmobarColor "#f38ba8" ""
-    marron    = xmobarColor "#eba0ac" ""
-    peach     = xmobarColor "#fab387" ""
-    yellow    = xmobarColor "#f9e2af" ""
-    green     = xmobarColor "#a6e3a1" ""
-    teal      = xmobarColor "#94e2d5" ""
-    sky       = xmobarColor "#89dceb" ""
-    sapphire  = xmobarColor "#74c7ec" ""
-    blue      = xmobarColor "#89b4fa" ""
-    lavender  = xmobarColor "#b4befe" ""
-    text      = xmobarColor "#cdd6f4" ""
-    base      = xmobarColor "#050508" ""
-    surface2  = xmobarColor "#585b70" ""
-
-
+-------------------------------------------------------------------------------
 myConfig = def
     {
-      modMask            = mod4Mask              -- Rebind Mod to the Super Key
+      modMask            = myModMask             -- Rebind Mod to the Super Key
     , layoutHook         = myLayoutHook          -- Use custom layouts
     , startupHook        = myStartupHook         -- Autostart applications
     , manageHook         = myManageHook          -- Match on certain windows
     , borderWidth        = myBorderWidth         -- Border width
     , normalBorderColor  = myNormalBorderColor   -- Normal border color
     , focusedBorderColor = myFocusedBorderColor  -- Focused border color
+    , focusFollowsMouse  = myFocusFollowsMouse
+    , clickJustFocuses   = myClickJustFocuses
     }
   `additionalKeysP` myKeys
 
+-------------------------------------------------------------------------------
 main :: IO ()
 main = xmonad
      . ewmhFullscreen
      . ewmh
-     . withEasySB (statusBarProp "xmobar" (pure myXmobarPP)) toggleStrutsKey
      $ myConfig
-  where
-    toggleStrutsKey :: XConfig Layout -> (KeyMask, KeySym)
-    toggleStrutsKey XConfig{ modMask = m } = (m, xK_s)
