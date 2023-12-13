2132b3a14a6058a8aa946ca9054df991c1ca33ef
Gabriel Lima <gabrielgsdlima@gmail.com>
2023-12-13 20:35:57 -0300

    Home-manager attr set

--- flake.nix
+++ flake.nix
@@ -6,8 +6,10 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
     # Home manager
-    home-manager.url = "github:nix-community/home-manager";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
     # Hyprland
     hyprland.url = "github:hyprwm/Hyprland";
