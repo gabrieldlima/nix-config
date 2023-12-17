a7bf894b0b5dfe9bc8eddb96c4da4f2809ae753c
Gabriel Lima <gabrielgsdlima@gmail.com>
2023-12-17 12:57:55 -0300

    Add colors variables and set the border color

--- home/gabriel/wayland/hyprland/default.nix
+++ home/gabriel/wayland/hyprland/default.nix
@@ -19,6 +19,36 @@
     xwayland.enable = true;
     settings = {
       ###############################################################################
+      # Theme: Catppuccin Mocha
+      ###############################################################################
+      "$rosewater" = "0xfff5e0dc";
+      "$flamingo"  = "0xfff2cdcd";
+      "$pink"      = "0xfff5c2e7";
+      "$mauve"     = "0xffcba6f7";
+      "$red"       = "0xfff38ba8";
+      "$maroon"    = "0xffeba0ac";
+      "$peach"     = "0xfffab387";
+      "$yellow"    = "0xfff9e2af";
+      "$green"     = "0xffa6e3a1";
+      "$teal"      = "0xff94e2d5";
+      "$sky"       = "0xff89dceb";
+      "$sapphire"  = "0xff74c7ec";
+      "$blue"      = "0xff89b4fa";
+      "$lavender"  = "0xffb4befe";
+      "$text"      = "0xffcdd6f4";
+      "$subtext1"  = "0xffbac2de";
+      "$subtext0"  = "0xffa6adc8";
+      "$overlay2"  = "0xff9399b2";
+      "$overlay1"  = "0xff7f849c";
+      "$overlay0"  = "0xff6c7086";
+      "$surface2"  = "0xff585b70";
+      "$surface1"  = "0xff45475a";
+      "$surface0"  = "0xff313244";
+      "$base"      = "0xff050508";
+      "$mantle"    = "0xff181825";
+      "$crust"     = "0xff11111b";
+
+      ###############################################################################
       # Monitors
       ###############################################################################
       monitor = "HDMI-A-2,1920x1080@60,0x0,1";
@@ -44,9 +74,9 @@
         # gaps between windows and monitor edges
         gaps_out = 10;
         # border color for the active window
-        "col.active_border" = "rgba(33ccffee) rgba(00ff99ee) 45deg";
+        "col.active_border" = "$lavender";
         # border color for inactive windows
-        "col.inactive_border" = "rgba(595959aa)";
+        "col.inactive_border" = "$base";
         # in seconds, after how many seconds of cursor’s inactivity to hide it. Set to 0 for never
         cursor_inactive_timeout = 0;
         # which layout to use. (Available: dwindle, master)
