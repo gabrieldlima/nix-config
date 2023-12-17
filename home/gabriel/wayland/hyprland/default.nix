ab9cdf5227ebe0acb0de1dd110d5066b0a066f3f
Gabriel Lima <gabrielgsdlima@gmail.com>
2023-12-17 12:31:34 -0300

    Cursor no warps

--- home/gabriel/wayland/hyprland/default.nix
+++ home/gabriel/wayland/hyprland/default.nix
@@ -52,7 +52,7 @@
         # which layout to use. (Available: dwindle, master)
         layout = "master";
         # if true, will not warp the cursor in many cases (focusing, keybinds, etc)
-        no_cursor_warps = false;
+        no_cursor_warps = true;
         # if true, will not fall back to the next available window when moving focus in a direction where no window was found
         no_focus_fallback = false;
         # if on, will also apply the sensitivity to raw mouse output (e.g. sensitivity in games) NOTICE: really not recommended
