9d8da06dc8f2208f8a878a2328a24c6bd2f490bf
Gabriel Lima <gabrielgsdlima@gmail.com>
2024-02-03 14:38:24 -0300

    Some firefox policies

--- home/gabriel/programs/firefox.nix
+++ home/gabriel/programs/firefox.nix
@@ -4,6 +4,17 @@
 }: {
   programs.firefox = {
     enable = true;
+    enableGnomeExtensions = false;
     package = pkgs.firefox;
+    policies = {
+      DefaultDownloadDirectory = "\${home}/Downloads";
+      DownloadDirectory = "\${home}/Downloads";
+      DisableBuiltinPDFViewer = true;
+      DisablePasswordReveal = true;
+      DisableProfileImport = true;
+      DisableTelemetry = true;
+      DontCheckDefaultBrowser = true;
+      PrintingEnabled = false;
+    };
   };
 }
