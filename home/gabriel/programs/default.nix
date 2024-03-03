023add039ea407bd4215724dd299b6146353d768
Gabriel Lima <gabrielgsdlima@gmail.com>
2024-03-02 21:11:19 -0300

    Move more files around

--- home/gabriel/programs/default.nix
+++ home/gabriel/programs/default.nix
@@ -3,20 +3,24 @@
   ...
 }: {
   imports = [
+    # ./hyprland
+    # ./xmonad
+    ./awesome.nix
     ./bat.nix
     ./eza.nix
     ./firefox.nix
     ./fish.nix
     ./fzf.nix
     ./git.nix
-    # ./hyprland
     ./kitty.nix
     ./mangohud.nix
+    ./picom.nix
     ./qutebrowser.nix
     ./starship.nix
     ./steam.nix
     ./tmux.nix
     ./wezterm.nix
+    ./xresources.nix
     ./zathura.nix
   ];
   home.packages = with pkgs; [
