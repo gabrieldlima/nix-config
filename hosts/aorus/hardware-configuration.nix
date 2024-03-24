ae41d255f148c8faf4eebd407dbe8936e74c758c
Gabriel Lima <gabrielgsdlima@gmail.com>
2024-03-23 21:49:48 -0300

    Change swap file size to 4GB

--- hosts/aorus/hardware-configuration.nix
+++ hosts/aorus/hardware-configuration.nix
@@ -37,7 +37,7 @@
 
   swapDevices = [ {
     device = "/var/lib/swapfile";
-    size = 8*1024;
+    size = 4*1024;
   }];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
