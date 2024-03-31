0e9c7eba093e281c243a860bfac82bf935a043bd
Gabriel Lima <gabrielgsdlima@gmail.com>
2024-03-31 16:19:13 -0300

    Bash config

---
+++ home/gabriel/programs/bash.nix
@@ -0,0 +1,20 @@
+{
+  pkgs,
+  ...
+}: {
+  programs.bash = {
+  enable = true;
+
+  # Extra commands that should be run when initializing a login shell.
+  profileExtra = ''
+    if [ -z "$DISPLAY" ] && [ "$XDG_VTNR" = 1 ]; then
+      exec startx $(which awesome)
+    fi
+  '';
+
+  # Extra commands that should be run when initializing an interactive shell.
+  initExtra = ''
+    NIX_LD= "$(nix eval --impure --raw --expr 'let pkgs = import <nixpkgs> {}; NIX_LD = pkgs.lib.fileContents '${pkgs.stdenv.cc}/nix-support/dynamic-linker'; in NIX_LD')";
+  '';
+  };
+}
