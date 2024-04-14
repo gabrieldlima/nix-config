d8f27c6bd162768e7450b74e6849027eb74f7af0
Gabriel Lima <gabrielgsdlima@gmail.com>
2024-04-14 13:39:47 -0300

    Update catppuccin theme rev

--- home/gabriel/programs/bat.nix
+++ home/gabriel/programs/bat.nix
@@ -9,8 +9,8 @@
         src = pkgs.fetchFromGitHub {
           owner = "catppuccin";
           repo = "bat";
-          rev = "ba4d16880d63e656acced2b7d4e034e4a93f74b1";
-          sha256 = "sha256-6WVKQErGdaqb++oaXnY3i6/GuH2FhTgK0v4TN4Y0Wbw=";
+          rev = "b8134f0";
+          sha256 = "sha256-gzf0/Ltw8mGMsEFBTUuN33MSFtUP4xhdxfoZFntaycQ=";
         };
         file = "Catppuccin-mocha.tmTheme";
       };
