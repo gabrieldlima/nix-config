03d1bd66da797d680a090355e816462b920ea3f8
Gabriel Lima <gabrielgsdlima@gmail.com>
2024-04-17 21:00:21 -0300

    Window padding and fonts

--- home/gabriel/programs/alacritty.nix
+++ home/gabriel/programs/alacritty.nix
@@ -5,7 +5,35 @@
   programs.alacritty = {
     enable = true;
     settings = {
+      # GENERAL
       shell = "fish";
+
+      # WINDOW
+      window = {
+        padding.x = 10;
+        padding.y = 10;
+      };
+
+      # FONT
+      font = {
+        normal = {
+          family = "FiraCode Nerd Font";
+          style = "Regular";
+        };
+        bold = {
+          family = "FiraCode Nerd Font";
+          style = "Bold";
+        };
+        italic = {
+          family = "FiraCode Nerd Font";
+          style = "Italic";
+        };
+        bold_italic = {
+          family = "FiraCode Nerd Font";
+          style = "Bold Italic";
+        };
+        size = 12;
+      };
     };
   };
 }
