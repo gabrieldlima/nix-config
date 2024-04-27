e7d9e52e9cb0cd9d9818ba1235d24e8802fad704
Gabriel Lima <gabrielgsdlima@gmail.com>
2024-04-27 20:59:23 -0300

    Catppuccin theme for zellij

--- home/gabriel/programs/zellij.nix
+++ home/gabriel/programs/zellij.nix
@@ -6,9 +6,25 @@
     enable = true;
     settings = {
       on_force_close = "quit";
-      simplified_ui = true;
+      simplified_ui = false;
       pane_frames = true;
       default_shel = "fish";
+      theme = "catppuccin-mocha-dark";
+      themes = {
+        catppuccin-mocha-dark =  {
+          bg      = "#010101";
+          fg      = "#cdd6f4";
+          red     = "#f38ba8";
+          green   = "#a6e3a1";
+          blue    = "#89b4fa";
+          yellow  = "#f9e2af";
+          magenta = "#f5c2e7";
+          orange  = "#fab387";
+          cyan    = "#89dceb";
+          black   = "#010101";
+          white   = "#cdd6f4";
+        };
+      };
     };
   };
 }
