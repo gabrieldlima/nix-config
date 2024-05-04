8dfb7a39f3fe4bca9ce18836ce54d556bba819c7
Gabriel Lima <gabrielgsdlima@gmail.com>
2024-05-04 19:18:52 -0300

    Wezterm: set opacity

--- home/gabriel/programs/wezterm.nix
+++ home/gabriel/programs/wezterm.nix
@@ -42,7 +42,7 @@
     extraConfig = ''
       return {
         color_scheme = "catppuccinMocha",
-        window_background_opacity = 1,
+        window_background_opacity = 0.95,
         default_prog = { 'fish' },
         font = wezterm.font 'FiraCode Nerd Font',
         font_size = 11.0,
